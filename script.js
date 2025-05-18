6c108ff3ba76e0f9b44f917b6271a4124450f70a
itshellboy <visalkr976@gmail.com>
2025-05-18 15:33:36 +0530

    completed

--- script.js
+++ script.js
@@ -26,18 +26,81 @@ const questionElement = document.getElementById('question');
 const choicesElement = document.getElementById('choices');
 const submitButton = document.getElementById('submit-btn');
 const resultElement = document.getElementById('result');
-
+const wrongAnswers = []
+let currenQuestion = 0
+let score = 0
 function loadQuestion() {
-  const { question, choices } = quizData[0]
-  questionContainer.textContent = question
-  // choicesElement.innerHtml = ""
-  choices.forEach((choice) => {
+  const { question, choices } = quizData[currenQuestion]
+  questionElement.textContent = question
+  choicesElement.innerHTML = ""
+  choices.forEach((choice, index) => {
     const btn = document.createElement('button')
     btn.textContent = choice
+    btn.addEventListener('click', () => selectIndex(index))
     console.log(btn)
     choicesElement.appendChild(btn)
-  })
+  });
 
 
 }
+function selectIndex(index) {
+  [...choicesElement.children].forEach(button => {
+    button.classList.remove('selected')
+  })
+  choicesElement.children[index].classList.add('selected')
+
+}
+function submitAnswer() {
+  const selectedButton = choicesElement.querySelector('.selected')
+  if (!selectedButton) return;
+  const selectedIndex = [...choicesElement.children].indexOf(selectedButton)
+  if (selectedIndex === quizData[currenQuestion].correctAnswer) {
+    score++
+  } else {
+    console.log(quizData[currenQuestion].correctAnswer)
+    wrongAnswers.push(
+      {
+        question: quizData[currenQuestion].question,
+        userAnswer: quizData[currenQuestion].choices[selectedIndex],
+        correctAnswer: quizData[currenQuestion].choices[
+          quizData[currenQuestion].currenQuestion
+        ],
+      }
+    )
+  }
+  currenQuestion++;
+  if (currenQuestion < quizData.length) {
+    loadQuestion()
+  } else {
+    showResults()
+  }
+
+}
+function showResults() {
+  questionContainer.style.display = 'none'
+  submitButton.style.display = 'none'
+  let resultHtml = `
+  <p class='score'>
+  You scored
+  ${Math.round((score * 100) / quizData.length)}%
+(${score} out of ${quizData.length})
+  </p>
+  `
+  if (wrongAnswers.length > 0) {
+    resultHtml += "<h3>Wrong Answers</h3>";
+    resultHtml += "<ul>";
+    wrongAnswers.forEach(answer => {
+      resultHtml += `
+      <li>
+      <p> Question : ${answer.question}</p>
+      <p> UserAnswer: ${answer.userAnswer}</p>
+      <p> CorrectAnswer: ${answer.correctAnswer} </p>
+      </li>
+      `
+    })
+    resultHtml += "</ul>"
+  }
+  resultElement.innerHTML = resultHtml
+}
+submitButton.addEventListener('click', submitAnswer)
 loadQuestion()
